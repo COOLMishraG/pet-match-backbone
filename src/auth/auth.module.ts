64f63054a86aea295ae62b67d1f7f1db92157b8f
COOLMishraG <anujmishra04@outlook.com>
2025-06-06 10:10:13 +0530

    web-auth setup successful

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { GoogleStrategy } from './google.strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'google' }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService , GoogleStrategy]
+})
+export class AuthModule {}
